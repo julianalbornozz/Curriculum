7d0b1229b22dce4a586304ab2af2dbaf774aa56c
julianalbornozz <126413184+julianalbornozz@users.noreply.github.com>
2023-02-27 23:30:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+alert("Bienvenidos al Proyecto");
+
+
+let miBoton = document.getElementById("miBoton");
+
+miBoton.addEventListener("click", redirigir)
+
+function redirigir() {
+    alert ("Sera redirigido a un sitio externo");
+}
+
+
